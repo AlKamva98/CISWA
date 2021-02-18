2bde114938d3adc96042201419e9c677eabe200a
AlKamva98 <60961380+AlKamva98@users.noreply.github.com>
2021-02-18 14:45:38 +0200

    Add files via upload

---
+++ CISWA/js/cheatsheet.js
@@ -0,0 +1,77 @@
+/* global bootstrap: false */
+
+(function () {
+  'use strict'
+
+  // Tooltip and popover demos
+  document.querySelectorAll('.tooltip-demo')
+    .forEach(function (tooltip) {
+      new bootstrap.Tooltip(tooltip, {
+        selector: '[data-bs-toggle="tooltip"]'
+      })
+    })
+
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+    .forEach(function (popover) {
+      new bootstrap.Popover(popover)
+    })
+
+  document.querySelectorAll('.toast')
+    .forEach(function (toastNode) {
+      var toast = new bootstrap.Toast(toastNode, {
+        autohide: false
+      })
+
+      toast.show()
+    })
+
+  // Disable empty links
+  document.querySelectorAll('[href="#"]')
+    .forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault()
+      })
+    })
+
+  function setActiveItem() {
+    var hash = window.location.hash
+
+    if (hash === '') {
+      return
+    }
+
+    var link = document.querySelector('.bd-aside a[href="' + hash + '"]')
+    var active = document.querySelector('.bd-aside .active')
+    var parent = link.parentNode.parentNode.previousElementSibling
+
+    link.classList.add('active')
+
+    if (parent.classList.contains('collapsed')) {
+      parent.click()
+    }
+
+    if (!active) {
+      return
+    }
+
+    var expanded = active.parentNode.parentNode.previousElementSibling
+
+    active.classList.remove('active')
+
+    if (expanded && parent !== expanded) {
+      expanded.click()
+    }
+  }
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
+
+  setActiveItem()
+  window.addEventListener('hashchange', setActiveItem)
+})()
